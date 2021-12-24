d48f11ca46e7596c2e9f604b959c59825901e2bd
phuongtran <phuongtran@lecle.co.kr>
2021-12-25 03:37:21 +0700

    fix: :bug: fix foodCount in category

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -14,11 +14,6 @@ module.exports = ({ strapi }) =>  ({
         params.populate = 'image';
       }
       const page = await strapi.service('api::category.category').find(params);
-      if (!_.isEmpty(page.results)) {
-        page.results.forEach((item) => {
-          item.foodCount = 0;
-        });
-      }
       return page;
     } catch (ex) {
       return ctx.badGateway(ex);
